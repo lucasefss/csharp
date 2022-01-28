abb446faa87652a81607d40d19cf253dba8637c7
lucasefss <lucasef96@gmail.com>
2022-01-28 15:10:55 -0300

    TestesDio

---
+++ volume.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace teste2
+{
+    class Lucas
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, volume;
+            pi = 3.14159;
+
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            volume = 4.0 / 3 * pi * Math.Pow(raio, 3);
+
+            Console.WriteLine("VOLUME = " + string.Format("{0:0.000}", volume));
+        }
+    }
+}
